4cd3a699c7dc93579fdfb3ce49c48acf0b651379
Romm1002 <romain.chaumont@hotmail.fr>
2022-12-09 12:36:44 +0100

    prbl bdd

--- migrations/Version20221209111348.php
+++ migrations/Version20221209111348.php
@@ -20,14 +20,12 @@ final class Version20221209111348 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE panier CHANGE user_id user_id INT NOT NULL');
         $this->addSql('ALTER TABLE user ADD date_inscription DATETIME NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE panier CHANGE user_id user_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE `user` DROP date_inscription');
     }
 }
