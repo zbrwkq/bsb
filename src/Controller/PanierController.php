4157541bb1199ba275774216bef63f92a80e7520
zbrwkq <Damien.chv78@gmail.com>
2022-12-09 16:31:13 +0100

    traduction flash

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -8,6 +8,7 @@ use App\Repository\PanierRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[Route('{_locale}')]
 class PanierController extends AbstractController
@@ -27,7 +28,7 @@ class PanierController extends AbstractController
      * Passe l'etat du panier actif de l'utilisateur a 1 et met à jour la date d'achat
      */
     #[Route('/panier/achat', name: 'app_panier_achat')]
-    public function achatPanier(EntityManagerInterface $em): Response
+    public function achatPanier(EntityManagerInterface $em, TranslatorInterface $translator): Response
     {
         $p = $this->getUser()->getActivePanier();
         if($p != null){
@@ -44,11 +45,11 @@ class PanierController extends AbstractController
             $em->persist($p);
             $em->flush();
     
-            $this->addFlash('success', 'Votre commande est validée');
+            $this->addFlash('success',$translator->trans('flash.panier.commande_validée'));
             return $this->redirectToRoute('app_commande', ['id' => $p->getId()]);
         }
 
-        $this->addFlash('danger', 'Une erreur est survenue');
+        $this->addFlash('danger', $translator->trans('flash.panier.error'));
         return $this->redirectToRoute('app_produit_index');
     }
 
